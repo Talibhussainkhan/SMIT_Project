486397663b3f14bd647056236399e35005923a44
Talib Hussain Khan <talibhk161@gmail.com>
2025-08-11 18:52:16 +0500

    added more functionality

--- firebase-product-dashboard/app.js
+++ firebase-product-dashboard/app.js
@@ -1,6 +1,6 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js'
-import { getAuth , createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js'
-import { getFirestore, collection, addDoc, serverTimestamp, getDocs, deleteDoc, doc, updateDoc , getDoc   } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js'
+import { getFirestore, collection, addDoc, serverTimestamp, getDocs, deleteDoc, doc, updateDoc, getDoc } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 const login = document.querySelector('#login');
 const signup = document.querySelector('.signUp');
@@ -29,59 +29,65 @@ function showMessage(msg) {
   messageEl.textContent = msg;
 }
 
-if(signup){
-    signup.addEventListener('click', async ()=>{
-      
-      let email = document.querySelector('#signup-email').value;
-      let password = document.querySelector('#signup-password').value;
-      let role = document.querySelector('#role').value;
-   try {
+if (signup) {
+  signup.addEventListener('click', async () => {
 
-    const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-    const user = userCredential.user;
+    let email = document.querySelector('#signup-email').value;
+    let password = document.querySelector('#signup-password').value;
+    try {
 
-    await updateProfile(user, { displayName : role });
-
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
       alert("User registered successfully!");
       window.location.href = './login.html';
-    
-   } catch (error) {
+
+    } catch (error) {
       showMessage(error.message, "error");
-    
-   }
-})}
 
-if(login){
-login.addEventListener('click', async()=>{
-  let email = document.querySelector('#login-email').value;
-  let password = document.querySelector('#login-password').value;
-  try {
-    const userCredential =  await signInWithEmailAndPassword(auth,email,password)
-     const user = userCredential.user;
-     if(user.displayName === 'admin'){
-      location.href = '/dashboard.html'
-     }else{
-      location.href = '/user.html'
-     }
-  } catch (error) {
-   showMessage(error.message) 
-  }
-})
-}
-
-if(logoutBtn){
-  logoutBtn.addEventListener('click', ()=>{
-    window.location.href='./login.html'
+    }
   })
 }
 
-const fetchData = async ()=>{
+if (login) {
+  login.addEventListener('click', async () => {
+    let email = document.querySelector('#login-email').value;
+    let password = document.querySelector('#login-password').value;
+    try {
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      const user = userCredential.user;
+      console.log(user);
+      localStorage.setItem('user', user.uid);
+      if (user.displayName === 'admin') {
+        location.href = '/dashboard.html'
+      } else {
+        location.href = '/user.html'
+      }
+    } catch (error) {
+      showMessage(error.message)
+    }
+  })
+}
+
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', () => {
+    signOut(auth).then(() => {
+      console.log('Sign-out successful.');
+      // Sign-out successful.`
+      window.location.href = './login.html'
+      localStorage.removeItem('user')
+    }).catch((error) => {
+      // An error happened.
+      console.error("Error signing out: ", error);
+    });
+  })
+}
+
+const fetchData = async () => {
   const querySnapshot = await getDocs(collection(db, "items"));
   let card = document.querySelector('#card');
   card.innerHTML = '';
   querySnapshot.forEach((doc) => {
-  const item = doc.data()
-  card.innerHTML += `
+    const item = doc.data()
+    card.innerHTML += `
      <div class="card" style="width: 18rem;">
     <img src=${item.productImage} class="card-img-top" alt="...">
      <div class="card-body">
@@ -93,81 +99,81 @@ const fetchData = async ()=>{
    </div>
  </div>
      `
-});
+  });
 }
-if(location.pathname === '/dashboard.html'){
+if (location.pathname === '/dashboard.html') {
   fetchData()
 }
 
-if(addProduct){
-  addProduct.addEventListener('click', async ()=>{
+if (addProduct) {
+  addProduct.addEventListener('click', async () => {
     let productName = document.querySelector('#productName').value;
     let price = document.querySelector('#productPrice').value;
     let productImage = document.querySelector('#productImage').value;
     let productDescription = document.querySelector('#productDescription').value;
-    if(productName === '' || price === '' || productImage === '' || productDescription === ''){
+    if (productName === '' || price === '' || productImage === '' || productDescription === '') {
       alert('Details Missings')
       return
     }
     try {
-  const docRef = await addDoc(collection(db, "items"), {
-    productName,
-    productImage,
-    price,
-    productDescription,
-    createdAt : serverTimestamp()
-  });
-  console.log("Document written with ID: ", docRef.id);
-  Swal.fire({
-  title: "Product added!",
-  icon: "success"
-       });
-       productName = '';
-       price = '';
-       productImage ='';
-       productDescription = '';
-       document.querySelector('#close').click();
-       fetchData();
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+      const docRef = await addDoc(collection(db, "items"), {
+        productName,
+        productImage,
+        price,
+        productDescription,
+        createdAt: serverTimestamp()
+      });
+      console.log("Document written with ID: ", docRef.id);
+      Swal.fire({
+        title: "Product added!",
+        icon: "success"
+      });
+      productName = '';
+      price = '';
+      productImage = '';
+      productDescription = '';
+      document.querySelector('#close').click();
+      fetchData();
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   })
 }
 
-const deleteItem = async (id)=>{
+const deleteItem = async (id) => {
   let confirm = window.confirm('Are you sure to delete this items?')
-  if(!confirm) return null;
+  if (!confirm) return null;
   await deleteDoc(doc(db, "items", id));
   fetchData()
 }
 
-const editItem = async(id)=>{
+const editItem = async (id) => {
   const docRef = doc(db, "items", id);
   const docSnap = await getDoc(docRef);
   editedId = id;
   if (docSnap.exists()) {
     let data = docSnap.data()
-   document.querySelector('#editName').value = data.productName;
-   document.querySelector('#editPrice').value = data.price;
-   document.querySelector('#editImage').value = data.productImage;
-   document.querySelector('#editDescription').value = data.productDescription;
-} else {
-  console.log("No such document!");
+    document.querySelector('#editName').value = data.productName;
+    document.querySelector('#editPrice').value = data.price;
+    document.querySelector('#editImage').value = data.productImage;
+    document.querySelector('#editDescription').value = data.productDescription;
+  } else {
+    console.log("No such document!");
+  }
 }
-}
-if(editProductBtn){
-  editProductBtn.addEventListener('click', async ()=>{
+if (editProductBtn) {
+  editProductBtn.addEventListener('click', async () => {
     let editName = document.querySelector('#editName').value
     let editPrice = document.querySelector('#editPrice').value
     let editImage = document.querySelector('#editImage').value
     let editDescription = document.querySelector('#editDescription').value
-    
+
     const itemRef = doc(db, "items", editedId);
     await updateDoc(itemRef, {
-     productName : editName,
-     productImage : editImage,
-     productDescription: editDescription,
-     price : editPrice
+      productName: editName,
+      productImage: editImage,
+      productDescription: editDescription,
+      price: editPrice
     });
     fetchData();
     document.querySelector('#closeEditBtn').click()
--- firebase-product-dashboard/user.js
+++ firebase-product-dashboard/user.js
@@ -1,4 +1,5 @@
-import { getFirestore, collection, addDoc, serverTimestamp, getDocs, deleteDoc, doc, updateDoc , getDoc   } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { getFirestore, collection, query, where, addDoc, getDocs, deleteDoc, doc, getDoc, updateDoc, writeBatch  } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+import { signOut, getAuth } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js'
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js'
 
 
@@ -14,14 +15,16 @@ const firebaseConfig = {
 };
 const app = initializeApp(firebaseConfig)
 const db = getFirestore(app);
+const auth = getAuth(app);
 
-const fetchData = async ()=>{
+const fetchData = async () => {
   const querySnapshot = await getDocs(collection(db, "items"));
   let card = document.querySelector('#card');
   card.innerHTML = '';
   querySnapshot.forEach((doc) => {
-  const item = doc.data()
-  card.innerHTML += `
+    const item = doc.data();
+
+    card.innerHTML += `
      <div class="card" style="width: 18rem;">
     <img src=${item.productImage} class="card-img-top" alt="...">
      <div class="card-body">
@@ -32,61 +35,174 @@ const fetchData = async ()=>{
    </div>
  </div>
      `
+  });
+}
+fetchData();
+
+// get user Already save carts items
+let items = []
+let uid = localStorage.getItem('user');
+const getAlreadyCartItem = async ()=>{
+  items = []
+  const cartRef = collection(db, "cart");
+const q = query(cartRef, where("uid", "==", uid));
+const querySnapshot = await getDocs(q);
+querySnapshot.forEach((doc) => {
+    items.push(doc.data())
 });
-}
-fetchData()
+};
+getAlreadyCartItem()
 
-const addToCart = async (id)=>{
-const docRef = doc(db, "items", id);
-  const docSnap = await getDoc(docRef);
-  if(docSnap.exists()){
-    let data = docSnap.data()
-    const docRef = await addDoc(collection(db, "cart"), {
-        data
-      });
-      alert('Item Added')
-      fetchCart()
-  }else {
-  console.log("No such document!");
-}
-}
-
-const fetchCart = async ()=>{
- const querySnapshot = await getDocs(collection(db, "cart"));
- let cartBody = document.querySelector('#cartBody');
- cartBody.innerHTML = '';
-  querySnapshot.forEach((doc) => {
-  const item = doc.data()
-  cartBody.innerHTML += 
-  `
-  <div class="cart-item">
-    <img src=${item.data.productImage}
-      alt="T Shirt For men" class="cart-img">
-     <div class="cart-details">
-            <h4 class="cart-name">${item.data.productName}</h4>
-            <p class="cart-description">${item.data.productDescription}</p>
-            <p class="cart-price">Price: <span>${item.data.price}</span></p>
-            </div>
-            <button onclick="deleteCartItem('${doc.id}')" class="delete-btn">🗑</button>
-    </div>
-  `
+const addToCart = async (id) => {
   
-});
+  // check item available in cart or not
+  // console.log(id)
+    let existItem = items.find(item => item.pid === id);
+    if(existItem){
+      // console.log('Already in cart')
+      alert('Item already in cart')
+      return
+    }
+  const docRef = doc(db, "items", id);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    let data = docSnap.data();
+     await addDoc(collection(db, "cart"), {
+      pid: docSnap.id,
+      uid,
+      price: data.price,
+      description: data.productDescription,
+      image: data.productImage,
+      productName: data.productName,
+      quantity : 1
+    });
+    alert('Item Added')
+    fetchCart();
+    getAlreadyCartItem()
+  } else {
+    console.log("No such document!");
+  }
+}
+
+const fetchCart = async () => {
+  let uid = localStorage.getItem('user');
+  const cartRef = collection(db, "cart");
+  const q = query(cartRef, where("uid", "==", uid));
+  try {
+    const querySnapshot = await getDocs(q);
+    cartBody.innerHTML = '';
+    querySnapshot.forEach((doc) => {
+      const item = doc.data()
+      cartBody.innerHTML +=
+        `
+    <div class="cart-item">
+  <img src=${item.image} alt="T Shirt For Men" class="cart-img">
+
+  <div class="cart-details">
+    <h4 class="cart-name">${item.productName}</h4>
+    <p class="cart-description">${item.description}</p>
+    <p class="cart-price">Price: <span>${item.price}</span></p>
+
+    <div class="quantity-control">
+      <button class="qty-btn" onclick="updateQuantity('${doc.id}', 'decrease')">-</button>
+      <span class="qty-value">${item.quantity || 1}</span>
+      <button class="qty-btn" onclick="updateQuantity('${doc.id}', 'increase')">+</button>
+    </div>
+  </div>
+
+  <button onclick="deleteCartItem('${doc.id}')" class="delete-btn">🗑</button>
+</div>
+    `
+    });
+    cartBody.innerHTML += `<button class='btn btn-primary' onClick='checkOut()'> Checkout </button>`
+
+    if (querySnapshot.empty) {
+      cartBody.innerHTML = `<h3 class="text-center">Cart is Empty</h3>`;
+      getAlreadyCartItem()
+      console.log("No matching documents.");
+    }
+  } catch (error) {
+    console.error("Error fetching documents: ", error);
+  }
 }
 fetchCart();
 
-const deleteCartItem = async (id)=>{
-    let confirm = window.confirm('Are you delete this item?')
-    if(!confirm) return null;
-    await deleteDoc(doc(db, "cart", id));
-    fetchCart()
+const deleteCartItem = async (id) => {
+  let confirm = window.confirm('Are you delete this item?')
+  if (!confirm) return null;
+  await deleteDoc(doc(db, "cart", id));
+  getAlreadyCartItem();
+  fetchCart();
 }
 
-if(logoutBtn){
-  logoutBtn.addEventListener('click', ()=>{
-    window.location.href='./login.html'
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', () => {
+    signOut(auth).then(() => {
+      console.log('Sign-out successful.');
+      // Sign-out successful.`
+      window.location.href = './login.html'
+      localStorage.removeItem('user')
+    }).catch((error) => {
+      // An error happened.
+      console.error("Error signing out: ", error);
+    });
   })
 }
 
+ const updateQuantity= async (docId, action) => {
+  const itemRef = doc(db, "cart", docId); // Reference to the cart item
+  const itemSnap = await getDoc(itemRef);
+  let currentQty = itemSnap.data().quantity;
+  
+  if(action === 'increase'){
+    currentQty += 1
+  }else{
+    currentQty -= 1;
+  }
+
+  if(currentQty === 0){
+  const confirm = window.confirm('If the quantity is less than 1, it is automatically deleted');
+  if(!confirm) return
+  await deleteDoc(itemRef);
+  fetchCart()
+  return
+  }
+   await updateDoc(itemRef, {
+      quantity: currentQty
+    });
+    // console.log(currentQty)
+   fetchCart()
+}
+
+const checkOut = async ()=>{
+  let uid = localStorage.getItem('user');
+  const cartRef = collection(db, "cart");
+  const q = query(cartRef, where("uid", "==", uid));
+
+   const querySnapshot = await getDocs(q);
+  
+   if (querySnapshot.empty) {
+    console.log("No documents found.");
+    return;
+  }
+    
+  await Promise.all(querySnapshot.docs.map(docSnap => deleteDoc(docSnap.ref)));
+ 
+  Swal.fire({
+    title: "Order Confirmed! 🎉",
+    text: "Your order has been placed successfully.",
+    icon: "success",
+    confirmButtonText: "OK",
+    confirmButtonColor: "#3085d6",
+    timer: 3000, // auto close after 3s
+    timerProgressBar: true
+  });
+
+  fetchCart();
+}
+
+
+window.checkOut = checkOut;
+window.updateQuantity = updateQuantity;
 window.deleteCartItem = deleteCartItem;
 window.addToCart = addToCart;
