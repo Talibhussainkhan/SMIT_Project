f4b8da680be8fce4c6263839e040fc06d6f1dac4
Talib Hussain Khan <talibhk161@gmail.com>
2025-08-31 20:34:48 +0500

    resolve import statement  react router project

--- react-router-dom-routing/src/App.jsx
+++ react-router-dom-routing/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/navbar'
+import Navbar from './components/Navbar'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import About from './pages/About'
